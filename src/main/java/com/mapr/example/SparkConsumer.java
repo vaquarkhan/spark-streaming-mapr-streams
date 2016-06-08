6c98bb49eb2626bc8ea3ca194f3d240d7c463ce1
Vince Gonzalez <vince@Vinces-MacBook-Pro.local>
2016-06-08 17:05:42 -0400

    adapt Spark sample code

--- src/main/java/com/mapr/example/SparkConsumer.java
+++ src/main/java/com/mapr/example/SparkConsumer.java
@@ -1,51 +1,68 @@
 package com.mapr.example;
+/**
+ * N.B. - this code lifted almost entirely from https://github.com/apache/spark/blob/master/examples/src/main/java/org/apache/spark/examples/streaming/JavaDirectKafkaWordCount.java
+ * and adapted as a minimal, self-contained example of how to use MapR Streams and Spark Streaming together.
+ */
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import scala.Tuple2;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.*;
-import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaPairInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-
+import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.kafka.v09.KafkaUtils;
-import scala.Tuple2;
+import org.apache.spark.streaming.Durations;
 
-import java.util.*;
-import java.util.regex.Pattern;
+/**
+ * Consumes messages from one or more topics in Kafka and does wordcount.
+ * Usage: JavaDirectKafkaWordCount <brokers> <topics>
+ *   <brokers> is a list of one or more Kafka brokers
+ *   <topics> is a list of one or more kafka topics to consume from
+ *
+ * Example:
+ *    $ bin/run-example streaming.JavaDirectKafkaWordCount broker1-host:port,broker2-host:port \
+ *      topic1,topic2
+ */
 
-
-public class SparkConsumer {
+public final class SparkConsumer {
   private static final Pattern SPACE = Pattern.compile(" ");
 
-  public static void main(String args[]) throws Exception {
-/*
-    Properties properties = new Properties();
-    properties.put("serializer.class", "org.apache.kafka.common.serialization.StringSerializer");
-    properties.put("key.serializer.class", "org.apache.kafka.common.serialization.StringSerializer");
-    properties.put("value.serializer.class", "org.apache.kafka.common.serialization.StringDeserializer");
-    properties.put("metadata.broker.list", "localhost:9092");
-    properties.put("request.required.acks", "1");
-*/
-    String brokers = "maprdemo:9092";
-    String topics = "/tmp/spark-test-stream:topic1";
+  public static void main(String[] args) throws Exception {
+    if (args.length < 2) {
+      System.err.println("Usage: JavaDirectKafkaWordCount <brokers> <topics>\n" +
+          "  <brokers> is a list of one or more Kafka brokers\n" +
+          "  <topics> is a list of one or more kafka topics to consume from\n\n");
+      System.exit(1);
+    }
 
-    SparkConf sparkConf = new SparkConf().setAppName("MapRStreams");
-    JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(30));
+    String brokers = args[0];
+    String topics = args[1];
 
-    Set<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
-    Map<String, String> kafkaParams = new HashMap<String, String>();
-    //kafkaParams.put("zookeeper.connect", "localhost:2181");
-    //kafkaParams.put("metadata.broker.list", brokers);
-    kafkaParams.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    kafkaParams.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    // Create context with a 2 seconds batch interval
+    SparkConf sparkConf = new SparkConf().setAppName("JavaDirectKafkaWordCount");
+    JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
+
+    Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+    Map<String, String> kafkaParams = new HashMap<>();
+    kafkaParams.put("metadata.broker.list", brokers);
 
     // Create direct kafka stream with brokers and topics
-    JavaPairInputDStream<String, String> directKafkaStream = KafkaUtils.createDirectStream(jssc, String.class, String.class, kafkaParams, topicsSet);
+    JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+        jssc,
+        String.class,
+        String.class,
+        kafkaParams,
+        topicsSet
+    );
+
     // Get the lines, split them into words, count the words and print
-    JavaDStream<String> lines = directKafkaStream.map(new Function<Tuple2<String, String>, String>() {
+    JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
       @Override
       public String call(Tuple2<String, String> tuple2) {
         return tuple2._2();
@@ -72,6 +89,7 @@ public class SparkConsumer {
         });
     wordCounts.print();
 
+    // Start the computation
     jssc.start();
     jssc.awaitTermination();
   }
